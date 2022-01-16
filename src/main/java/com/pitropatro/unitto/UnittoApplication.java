b5bda44a1cb6cbbe3a5fe24647b37d4702ec7352
hywook4 <hywook4@gmail.com>
2022-01-16 13:28:01 +0900

    Init unitto backend project

---
+++ src/main/java/com/pitropatro/unitto/UnittoApplication.java
@@ -0,0 +1,13 @@
+package com.pitropatro.unitto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnittoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnittoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pitropatro/unitto/UnittoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitropatro.unitto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnittoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
